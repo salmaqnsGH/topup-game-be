e66a6fec8a2abdbd11f4d20981fa9a4030a16cd3
salmaqnsGH <nurussalamahqonaah@gmail.com>
2023-01-13 20:37:38 +0700

    install method-override and update category

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var methodOverride = require('method-override')
 var logger = require('morgan');
 
 var categoryRouter = require('./app/category/router');
@@ -13,6 +14,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(methodOverride('_method'))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
