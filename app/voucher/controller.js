dd10fead2ae4a8815b9605f31a128e65ab440e65
salmaqnsGH <nurussalamahqonaah@gmail.com>
2023-01-14 22:04:06 +0700

    delete voucher

--- app/voucher/controller.js
+++ app/voucher/controller.js
@@ -150,4 +150,24 @@ module.exports={
             res.redirect('/voucher')
         }
     },
+    actionDelete: async(req,res)=>{
+        try{
+            const {id} = req.params
+
+            const voucher = await Voucher.findOneAndRemove({_id:id})
+
+            let currentImage = `${config.rootPath}/public/uploads/${voucher.thumbnail}`
+            if(fs.existsSync(currentImage)){
+                fs.unlinkSync(currentImage)
+            }
+            req.flash('alertMessage', 'Berhasil menghapus voucher')
+            req.flash('alertStatus', 'success')
+
+            res.redirect('/voucher')
+        }catch(err){
+            req.flash('alertMessage', `${err.message}`)
+            req.flash('alertStatus', 'danger')
+            res.redirect('/voucher')
+        }
+    }
 }
