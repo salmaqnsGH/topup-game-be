aff5fc2fa0ff8a68441159a620a545412f73e482
salmaqnsGH <nurussalamahqonaah@gmail.com>
2023-01-15 09:35:36 +0700

    edit bank

--- app/bank/controller.js
+++ app/bank/controller.js
@@ -42,5 +42,34 @@ module.exports={
             res.redirect('/bank')
         }
     },
+    viewEdit: async(req,res)=>{
+        try{
+            const {id} = req.params
+            const bank = await Bank.findOne({_id: id})
+
+            res.render('admin/bank/edit',{bank})
+        }catch(err){
+            req.flash('alertMessage', `${err.message}`)
+            req.flash('alertStatus', 'danger')
+            res.redirect('/bank')
+        }
+    },
+    actionEdit: async(req,res)=>{
+        try{
+            const {id} = req.params
+            const { name, bankName, noRekening } = req.body
+
+            const bank = await Bank.findOneAndUpdate({_id:id}, {name, bankName, noRekening})
+
+            req.flash('alertMessage', 'Berhasil mengubah bank')
+            req.flash('alertStatus', 'success')
+
+            res.redirect('/bank')
+        }catch(err){
+            req.flash('alertMessage', `${err.message}`)
+            req.flash('alertStatus', 'danger')
+            res.redirect('/bank')
+        }
+    },
     
 }
