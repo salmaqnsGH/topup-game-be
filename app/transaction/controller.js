af8caa9104fc55e06c1aae0a7df79c4a7f66f0bf
salmaqnsGH <nurussalamahqonaah@gmail.com>
2023-01-17 17:09:18 +0700

    update transaction status

--- app/transaction/controller.js
+++ app/transaction/controller.js
@@ -21,4 +21,21 @@ module.exports={
             res.redirect('/transaction')
         }
     },
+    actionStatus: async(req,res)=>{
+        try{
+            const {id} = req.params
+            const {status} = req.query
+
+            await Transaction.findOneAndUpdate({_id:id},{status})
+
+            req.flash('alertMessage', 'Status berhasil diubah')
+            req.flash('alertStatus', 'success')
+            res.redirect('/transaction')
+
+        }catch(err){
+            req.flash('alertMessage', `${err.message}`)
+            req.flash('alertStatus', 'danger')
+            res.redirect('/transaction')
+        }
+    }
 }
