0cce4ce1531ab5cba58f35d297d1d19197de691a
salmaqnsGH <nurussalamahqonaah@gmail.com>
2023-01-17 12:19:59 +0700

    installed bcryptjs and generate actionSignin

--- app/users/controller.js
+++ app/users/controller.js
@@ -1,3 +1,5 @@
+const User = require('./model')
+const bcrypt = require('bcryptjs')
 
 module.exports={
     viewSignin: async(req,res)=>{
@@ -10,6 +12,39 @@ module.exports={
         }catch(err){
             req.flash('alertMessage', `${err.message}`)
             req.flash('alertStatus', 'danger')
+            res.redirect('/')
         }
     },
+    actionSignin: async(req,res) => {
+        try{
+            const { email, password} = req.body
+            const getUser = await User.findOne({ email: email })
+
+            if(getUser){
+                if(getUser.status === 'Y'){
+                    const checkPassword = await bcrypt.compare(password, getUser.password)
+                    if(checkPassword){
+                        res.redirect('/dashboard')
+                    }else{
+                        req.flash('alertMessage', "kata sandi salah")
+                        req.flash('alertStatus', 'danger')
+                        res.redirect('/')
+                    }
+                }else{
+                    req.flash('alertMessage', "status belum aktif")
+                    req.flash('alertStatus', 'danger')
+                    res.redirect('/')
+                }
+            }else{
+                req.flash('alertMessage', "email tidak ditemukan")
+                req.flash('alertStatus', 'danger')
+                res.redirect('/')
+            }
+            
+        }catch(err){
+            req.flash('alertMessage', `${err.message}`)
+            req.flash('alertStatus', 'danger')
+            res.redirect('/')
+        }
+    }
 }
