dd6a5b7f41a4e88df2ce950195d46e6a5f1133d7
salmaqnsGH <nurussalamahqonaah@gmail.com>
2023-01-18 15:54:16 +0700

    create api for detail history

--- app/player/controller.js
+++ app/player/controller.js
@@ -143,4 +143,19 @@ module.exports={
             res.status(500).json({ message: err.message || `internal server error` })
         }
     },
+    historyDetail: async(req, res)=>{
+        try{
+            const {id} = req.params
+            const history = await Transaction.findOne({ _id: id })
+
+            if(!history) return res.status(404).json({ message: 'History tidak ditemukan' })
+
+            res.status(200).json({
+                data: history
+            })
+
+        }catch(err){
+            res.status(500).json({ message: err.message || `internal server error` })
+        }
+    },
 }
