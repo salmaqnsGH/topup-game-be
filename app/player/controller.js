14994f0c09ca344ef2ec5f0628011730be7d816a
salmaqnsGH <nurussalamahqonaah@gmail.com>
2023-01-18 15:41:55 +0700

    create api for checkout with jsonwebtoken

--- app/player/controller.js
+++ app/player/controller.js
@@ -1,6 +1,9 @@
-const Player = require('./model')
 const Voucher = require('../voucher/model')
 const Category = require('../category/model')
+const Bank = require('../bank/model')
+const Payment = require('../payment/model')
+const Nominal = require('../nominal/model')
+const Transaction = require('../transaction/model')
 
 module.exports={
     landingPage: async(req,res)=>{
@@ -38,5 +41,66 @@ module.exports={
         }catch{
             res.status(500).json({message: err.message || 'terjadi kesalahan pada server'})
         }
-    }
+    },
+    checkout: async(req,res)=>{
+        try{
+            const { accountUser, name, nominal, voucher, payment, bank } = req.body
+            const res_voucher = await Voucher.findOne({_id: voucher})
+            .select('name category thumbnail _id user')
+            .populate('category')
+            .populate('user')
+
+            if(!res_voucher) return res.status(404).json({ message: 'Voucher game tidak ditemukan' })
+
+            const res_nominal = await Nominal.findOne({ _id: nominal })
+            if(!res_nominal) return res.status(404).json({ message: 'Nominal tidak ditemukan' })
+
+            const res_payment = await Payment.findOne({ _id: payment })
+            if(!res_payment) return res.status(404).json({ message: 'Payment tidak ditemukan' })
+            
+            const res_bank = await Bank.findOne({ _id: bank })
+            if(!res_bank) return res.status(404).json({ message: 'Bank tidak ditemukan' })
+            
+            //menghitung pajak 10%
+            const tax = ( 10/100 ) * res_nominal._doc.price
+            //menghitung value
+            const value = res_nominal._doc.price - tax
+
+            //input history payment
+            const payload = {
+                historyVoucherTopup :{
+                    gameName: res_voucher._doc.name,
+                    category: res_voucher._doc.category ? res_voucher._doc.category.name : '',
+                    thumbnail: res_voucher._doc.thumbnail,
+                    coinName: res_nominal._doc.coinName,
+                    coinQuantity: res_nominal._doc.coinQuantity,
+                    price: res_nominal._doc.price
+                },
+                historyPayment:{
+                    name: res_bank._doc.name,
+                    type: res_payment._doc.type,
+                    bankName: res_bank._doc.bankName,
+                    noRekening: res_bank._doc.noRekening,
+                },
+                name: name,
+                accountUser: accountUser,
+                tax: tax,
+                value: value,
+                player: req.player._id,
+                category: res_voucher._doc.category?._id,
+                user: res_voucher._doc.user?._id,
+                historyUser:{
+                    name: res_voucher._doc.user?.name,
+                    phoneNumber: res_voucher._doc.user?.phoneNumber,
+                }
+            }
+            const transaction = new Transaction(payload)
+
+            await transaction.save()
+
+            res.status(201).json({ data: transaction })
+        }catch(err){
+            res.status(500).json({message: err.message || `Internal server error`})
+        }
+    },
 }
