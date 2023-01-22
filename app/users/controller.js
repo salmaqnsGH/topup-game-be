61a03a43dbbf3651770dd931726ee1581f2ec2f2
salmaqnsGH <nurussalamahqonaah@gmail.com>
2023-01-22 15:49:01 +0700

    initial readme

--- app/users/controller.js
+++ app/users/controller.js
@@ -1,68 +1,67 @@
-const User = require('./model')
-const bcrypt = require('bcryptjs')
+const User = require("./model");
+const bcrypt = require("bcryptjs");
 
-module.exports={
-    viewSignin: async(req,res)=>{
-        try{
-            const alertMessage = req.flash('alertMessage')
-            const alertStatus = req.flash('alertStatus')
-            const alert = { message: alertMessage, status: alertStatus }
+module.exports = {
+	viewSignin: async (req, res) => {
+		try {
+			const alertMessage = req.flash("alertMessage");
+			const alertStatus = req.flash("alertStatus");
+			const alert = { message: alertMessage, status: alertStatus };
 
-            if(req.session.user === null || req.session.user === undefined){
-                res.render('admin/users/view_signin',{
-                    alert,
-                    title: "Halaman Sign In"
-                })
-            }else{
-                res.redirect('/dashboard')
-            }
-        }catch(err){
-            req.flash('alertMessage', `${err.message}`)
-            req.flash('alertStatus', 'danger')
-            res.redirect('/')
-        }
-    },
-    actionSignin: async(req,res) => {
-        try{
-            const { email, password} = req.body
-            const getUser = await User.findOne({ email: email })
-
-            if(getUser){
-                if(getUser.status === 'Y'){
-                    const checkPassword = await bcrypt.compare(password, getUser.password)
-                    if(checkPassword){
-                        //simpan session
-                        req.session.user={
-                            id: getUser.id,
-                            name: getUser.name,
-                            email: getUser.email,
-                            status: getUser.status,
-                        }
-                        res.redirect('/dashboard')
-                    }else{
-                        req.flash('alertMessage', "kata sandi salah")
-                        req.flash('alertStatus', 'danger')
-                        res.redirect('/')
-                    }
-                }else{
-                    req.flash('alertMessage', "status belum aktif")
-                    req.flash('alertStatus', 'danger')
-                    res.redirect('/')
-                }
-            }else{
-                req.flash('alertMessage', "email tidak ditemukan")
-                req.flash('alertStatus', 'danger')
-                res.redirect('/')
-            }
-            
-        }catch(err){
-            req.flash('alertMessage', `${err.message}`)
-            req.flash('alertStatus', 'danger')
-            res.redirect('/')
-        }
-    },
-    actionLogout: async(req,res) =>{
-        req.session.destroy()
-        res.redirect('/')
-    }
-}
+			if (req.session.user === null || req.session.user === undefined) {
+				res.render("admin/users/view_signin", {
+					alert,
+					title: "Halaman Sign In",
+				});
+			} else {
+				res.redirect("/dashboard");
+			}
+		} catch (err) {
+			req.flash("alertMessage", `${err.message}`);
+			req.flash("alertStatus", "danger");
+			res.redirect("/");
+		}
+	},
+	actionSignin: async (req, res) => {
+		try {
+			const { email, password } = req.body;
+			const getUser = await User.findOne({ email: email });
+			console.log(getUser);
+			if (getUser) {
+				if (getUser.status === "Y") {
+					const checkPassword = await bcrypt.compare(password, getUser.password);
+					if (checkPassword) {
+						//simpan session
+						req.session.user = {
+							id: getUser.id,
+							name: getUser.name,
+							email: getUser.email,
+							status: getUser.status,
+						};
+						res.redirect("/dashboard");
+					} else {
+						req.flash("alertMessage", "kata sandi salah");
+						req.flash("alertStatus", "danger");
+						res.redirect("/");
+					}
+				} else {
+					req.flash("alertMessage", "status belum aktif");
+					req.flash("alertStatus", "danger");
+					res.redirect("/");
+				}
+			} else {
+				req.flash("alertMessage", "email tidak ditemukan");
+				req.flash("alertStatus", "danger");
+				res.redirect("/");
+			}
+		} catch (err) {
+			req.flash("alertMessage", `${err.message}`);
+			req.flash("alertStatus", "danger");
+			res.redirect("/");
+		}
+	},
+	actionLogout: async (req, res) => {
+		req.session.destroy();
+		res.redirect("/");
+	},
+};
